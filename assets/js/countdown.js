9de2e0da918934a76168a11abb6b1fada9da2e9a
webmediart-github <github@webmediart.de>
2021-03-20 23:33:43 +0100

    Update countdown.js

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,28 +1,23 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 28, 2022 14:00:00").getTime();
+var countDownDate = new Date("May 28, 2022 00:00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// Get today's date and time
+var now = new Date().getTime();
 
-  // Get today's date and time
-  var now = new Date().getTime();
+// Find the distance between now and the count down date
+var distance = countDownDate - now;
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+// Time calculations for remaining days
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="countdown"
-  document.getElementById("countdown").innerHTML = days + "Tage ";
-//  + hours + "h "  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
-  }
-}, 1000);
+// Display the result in the element with id="countdown"
+if (days < -1) {
+	document.getElementById("countdown").innerHTML = "<b>Vielen Dank für die schöne Feier mit Euch!</b>";
+}else if (days == -1){
+	document.getElementById("countdown").innerHTML = "Heute ist es soweit!";
+}else if (days == 0){
+	document.getElementById("countdown").innerHTML = "Morgen ist es soweit!";
+}else{
+	document.getElementById("countdown").innerHTML = "In <b>" + days + "</b> Tagen ist es soweit!";
+	//  + hours + "h "  + minutes + "m " + seconds + "s ";
+}
